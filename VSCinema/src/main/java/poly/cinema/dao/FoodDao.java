986f2f9b3f39d6d17a3d2a96f9970c9bcf037b66
khueho <khueho11a3@gmail.com>
2021-12-07 10:32:59 +0700

    95%

--- VSCinema/src/main/java/poly/cinema/dao/FoodDao.java
+++
@@ -1,9 +0,0 @@
-package poly.cinema.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import poly.cinema.entity.Food;
-
-public interface FoodDao extends JpaRepository<Food, Integer> {
-
-}
