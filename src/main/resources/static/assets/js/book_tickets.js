8d0d24a29c042494ec3fb6f22adfeabc4df31537
khueho <88199756+khueho@users.noreply.github.com>
2021-12-20 14:37:02 +0700

    Update book_tickets.js

--- src/main/resources/static/assets/js/book_tickets.js
+++ src/main/resources/static/assets/js/book_tickets.js
@@ -705,7 +705,7 @@ app.controller("booking-ticket-ctrl" , function($scope , $http){
 		$http.get("/rest/orders/checkOrder").then(resp => {
 			$scope.listOrder = resp.data;
 			console.log($scope.listOrder)
-		}).catch();
+		}).catch(error => {console.log(error)});
 	}
 	
 	$scope.pager = {
